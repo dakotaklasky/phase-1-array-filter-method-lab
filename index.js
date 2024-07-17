d400ecdf3927ae31c21c27ade2935cc7b6427c78
Dakota <dakota.klasky@gmail.com>
2024-07-17 14:46:47 -0400

    create functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter((driver) => {
+        return driver.slice(0,name.length).toLowerCase() === name.toLowerCase();
+    })
+}
+
+function matchName(drivers, nameSearch){
+    return drivers.filter((driver) => {
+        return driver.name.toLowerCase() === nameSearch.toLowerCase()
+    })
+
+}
